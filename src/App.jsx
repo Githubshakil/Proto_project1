1e5de2f45f9ad447c51e28dfb485ff0d3d872db9
Githubshakil <shakilmahmud007@gmail.com>
2025-04-05 17:09:16 +0600

    setup complete

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import HomeIndex from "./components/pages/home/HomeIndex"
+
+
+function App() {
+
+  return (
+    <>
+     <HomeIndex/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/pages/home/HomeIndex.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../layouts/Navbar'
+
+const HomeIndex = () => {
+  return (
+    <>
+    <Navbar/>
+    
+
+    </>
+  )
+}
+
+export default HomeIndex
